1d110d2ffa9362a57357c8e3c92fc55851653fd7
Bob Witmer <bwitmer@golden.net>
2025-08-08 09:19:22 -0400

    Initial Commit

---
+++ BipTheGuyAVP/BipTheGuyAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyAVPApp.swift
+//  BipTheGuyAVP
+//
+//  Created by Bob Witmer on 2025-08-08.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
